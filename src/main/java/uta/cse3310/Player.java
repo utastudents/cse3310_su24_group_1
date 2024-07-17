c48fc0b37cfd6016c7ad4e3018297deb892143ec
edencse <eia2614@mavs.uta.edu>
2024-07-17 16:55:49 -0500

    commit 2 files

---
+++ src/main/java/uta/cse3310/Player.java
@@ -0,0 +1,61 @@
+public class Player {
+   
+    private String playerName;
+    private String playerID;
+    private int points;
+    private boolean statusPlayer;
+
+    public Player(String playerName, String playerID) {
+        this.playerName = playerName;
+        this.playerID = playerID;
+        this.points = 0;
+        this.statusPlayer = false; 
+    }
+
+    // Methods
+    public String displayName() {
+   
+    }
+
+    public int displayScore() {
+        
+    }
+
+    public boolean getStatus() {
+       
+    }
+
+   
+    public String getPlayerName() {
+        
+    }
+
+    public void setPlayerName(String playerName) {
+       
+    }
+
+    public String getPlayerID() {
+    
+    }
+
+    public void setPlayerID(String playerID) {
+      
+    }
+
+    public int getPoints() {
+       
+    }
+
+    public void setPoints(int points) {
+     
+    }
+
+    public boolean isStatusPlayer() {
+       
+    }
+
+    public void setStatusPlayer(boolean statusPlayer) {
+       
+    }
+}
+
---
+++ src/main/java/uta/cse3310/Round.java
@@ -0,0 +1,79 @@
+import java.time.LocalDateTime;
+
+public class Round {
+  
+    private int roundNumber;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private int score;
+    private String status;
+
+  
+    public Round(int roundNumber) {
+        this.roundNumber = roundNumber;
+        this.startTime = null;
+        this.endTime = null;
+        this.score = 0;
+        this.status = "not started";
+    }
+
+
+    public void startRound() {
+        
+    }
+
+    public void endRound() {
+
+    }
+
+    public int calculateScore() {
+        
+    }
+
+    public String getStatus() {
+       
+    }
+
+    
+    public int getRoundNumber() {
+        
+    }
+
+    public void setRoundNumber(int roundNumber) {
+       
+    }
+
+    public LocalDateTime getStartTime() {
+       
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+    
+    }
+
+    public LocalDateTime getEndTime() {
+     
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+      
+    }
+
+    public int getScore() {
+   
+    }
+
+    public void setScore(int score) {
+   
+    }
+
+    public String getStatus() {
+     
+    }
+
+    public void setStatus(String status) {
+       
+    }
+}
+
+
