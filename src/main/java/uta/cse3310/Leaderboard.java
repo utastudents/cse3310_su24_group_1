8408793243fa79f36734e8fac0dd0081f049f982
uta-1001966434 <jxb6434@mavs.uta.edu>
2024-07-17 21:47:05 -0500

    Update and rename Leaderboard to Leaderboard.java

---
+++ src/main/java/uta/cse3310/Leaderboard.java
@@ -0,0 +1,13 @@
+public class Leaderboard {
+   
+    private String playerScores[][]; 
+
+    // Methods
+    public void sortPlayerScore(playerScores[][]) {
+        
+    }
+
+    public void displayPlayerScores() {
+       
+    }
+}
