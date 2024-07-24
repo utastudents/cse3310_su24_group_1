c19577a6d660330fb13e8253a6a26de60c15b67c
edencse <eia2614@mavs.uta.edu>
2024-07-24 18:44:13 -0500

    Provided the implementation of the Player and Round classes

--- src/main/java/uta/cse3310/Player.java
+++ src/main/java/uta/cse3310/Player.java
@@ -1,5 +1,5 @@
 public class Player {
-   
+
     private String playerName;
     private String playerID;
     private int points;
@@ -9,53 +9,52 @@ public class Player {
         this.playerName = playerName;
         this.playerID = playerID;
         this.points = 0;
-        this.statusPlayer = false; 
+        this.statusPlayer = false;
     }
 
     // Methods
     public String displayName() {
-   
+        return "Player Name: " + this.playerName;
     }
 
     public int displayScore() {
-        
+        return this.points;
     }
 
     public boolean getStatus() {
-       
+        return this.statusPlayer;
     }
 
-   
     public String getPlayerName() {
-        
+        return this.playerName;
     }
 
     public void setPlayerName(String playerName) {
-       
+        this.playerName = playerName;
     }
 
     public String getPlayerID() {
-    
+        return this.playerID;
     }
 
     public void setPlayerID(String playerID) {
-      
+        this.playerID = playerID;
     }
 
     public int getPoints() {
-       
+        return this.points;
     }
 
     public void setPoints(int points) {
-     
+        this.points = points;
     }
 
     public boolean isStatusPlayer() {
-       
+        return this.statusPlayer;
     }
 
     public void setStatusPlayer(boolean statusPlayer) {
-       
+        this.statusPlayer = statusPlayer;
     }
 }
 
--- src/main/java/uta/cse3310/Round.java
+++ src/main/java/uta/cse3310/Round.java
@@ -1,14 +1,13 @@
 import java.time.LocalDateTime;
 
 public class Round {
-  
+
     private int roundNumber;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
     private int score;
     private String status;
 
-  
     public Round(int roundNumber) {
         this.roundNumber = roundNumber;
         this.startTime = null;
@@ -17,62 +16,65 @@ public class Round {
         this.status = "not started";
     }
 
-
+    
     public void startRound() {
-        
+        this.startTime = LocalDateTime.now();
+        this.status = "in progress";
     }
 
     public void endRound() {
-
+        this.endTime = LocalDateTime.now();
+        this.status = "completed";
+        this.score = calculateScore(); // Assuming score is calculated at the end of the round
     }
 
     public int calculateScore() {
-        
+      
+        if (this.startTime != null && this.endTime != null) {
+            return (int) java.time.Duration.between(this.startTime, this.endTime).getSeconds();
+        } else {
+            return 0;
+        }
     }
 
     public String getStatus() {
-       
+        return this.status;
     }
 
-    
     public int getRoundNumber() {
-        
+        return this.roundNumber;
     }
 
     public void setRoundNumber(int roundNumber) {
-       
+        this.roundNumber = roundNumber;
     }
 
     public LocalDateTime getStartTime() {
-       
+        return this.startTime;
     }
 
     public void setStartTime(LocalDateTime startTime) {
-    
+        this.startTime = startTime;
     }
 
     public LocalDateTime getEndTime() {
-     
+        return this.endTime;
     }
 
     public void setEndTime(LocalDateTime endTime) {
-      
+        this.endTime = endTime;
     }
 
     public int getScore() {
-   
+        return this.score;
     }
 
     public void setScore(int score) {
-   
-    }
-
-    public String getStatus() {
-     
+        this.score = score;
     }
 
     public void setStatus(String status) {
-       
+        this.status = status;
     }
 }
 
