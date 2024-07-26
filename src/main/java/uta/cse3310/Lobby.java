22845a226899b53f89924bdcddfdd6f68c8c633e
Andy <andytieu325@gmail.com>
2024-07-26 15:42:02 -0500

    Added constructor, getters, and setters to allow for better variable manipulation

--- src/main/java/uta/cse3310/Lobby.java
+++ src/main/java/uta/cse3310/Lobby.java
@@ -3,14 +3,24 @@ package uta.cse3310;
 import java.util.List;
 
 public class Lobby {
-    List<Player> players;
+    public List<Player> players;
+    private Boolean gameStatus;
     private int playerCount;
+    private int lobbyId;
     private Leaderboard leaderboard;
+
+    public Lobby() {
+        this.players = null;
+        this.gameStatus = false;
+        this.playerCount = 0;
+        this.lobbyId = 0;
+        this.leaderboard = new Leaderboard();
+    }
 
     // allows for game to start once there are at least 2 players in lobby
     public void gameStart() {
         if(playerCount >= 2 && playerCount <= 4) {
-            Gameplay g = new Gameplay();
+            Gameplay G = new Gameplay();
         }
     }
 
@@ -25,5 +35,21 @@ public class Lobby {
     // displays leaderboard on the side of lobby to show all players' scores from server
     public void displayLeaderboard() {
         
+    }
+
+    public int getLobbyId() {
+        return lobbyId;
+    }
+
+    public void setLobbyId(int lobbyId) {
+        this.lobbyId = lobbyId;
+    }
+
+    public int getPlayerCount() {
+        return playerCount;
+    }
+
+    public Boolean getGameStatus() {
+        return gameStatus;
     }
 }
