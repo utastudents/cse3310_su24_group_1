472b9c2ce86e8aa4f59f857380f08737abe3f92a
unknown <barbozajoaquin73@gmail.com>
2024-07-29 15:33:11 -0500

    Gameplay Change

--- src/main/java/uta/cse3310/Gameplay.java
+++ src/main/java/uta/cse3310/Gameplay.java
@@ -7,21 +7,54 @@ public class Gameplay {
     private String madeGuesses;
     private Player player;
 
+    PlayerType Players;
+
     // Methods
-    public boolean answerCheck(String userGuess) {
-        return false;
+    public void playerInputGuess(player, User_Input) //input is the player and the input
+    {
+        /*Pseudo code 
+        userGuess = player.User_Input();
+        private string madeGuesses = userguesses 
+        */
     }
 
-    public void displayCorrect() {
-        
+public boolean answerCheck(String userGuess) {
+    public displayResponse()
+    {
+        /*
+         * if userGuess == correct character in the phrase
+         * userScore++;
+         * display.guesscorrect();
+         * else if (purchaseVowel)
+         * display a vowel
+         * else
+         * display.guessincorrect();
+         * skip
+         */
     }
-
-    public void displayIncorrect() {
-       
-    }
+}
 
    
-    public void switchPlayer() {
-        
+    public void switchPlayer(Players) {
+        /*
+         * change player value to the next one
+         * so player = player[1];
+         */
+    }
+
+    public void purchaseVowel(Players, playerBankAccount)
+    {
+        /*
+         * if player pushes button to purchase a vowel
+         * answer check will display a correct character in the phrase that is a vowel
+         * deduct the amount from the playerBankAccount
+         */
+    }
+
+    public void userStake(Players, playerInventory)
+    {
+        /*
+         * 
+         */
     }
 }
