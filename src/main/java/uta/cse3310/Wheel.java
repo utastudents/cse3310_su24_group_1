e67e06f6d63885dc8a94ea6c5b7fbec815cd9b69
juntinuous <yxl1510@mavs.uta.edu>
2024-07-29 15:44:37 -0500

    added wheel functionality

--- src/main/java/uta/cse3310/Wheel.java
+++ src/main/java/uta/cse3310/Wheel.java
@@ -1,9 +1,19 @@
 package uta.cse3310;
+import java.util.Random;
 
 public class Wheel {
-    private String[] item;
+    private String[] item = {"money", "bankrupt_another_player", "other"};
+
 
     public String getRandomItem() {
-        return null;
+
+            Random random = new Random();
+           int random_number = random.nextInt(2); // randome nubmer between 0~2
+           String random_item = item[random_number];
+    
+            System.out.print(random_item);
+            return random_item;
+            
+        
     }
 }
