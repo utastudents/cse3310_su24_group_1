275f7e0aceeb439745c849cd6771a13396221d30
andy-tieu <andytieu325@gmail.com>
2024-08-03 17:46:51 -0500

    Added 20 different items to the wheel

--- src/main/java/uta/cse3310/Wheel.java
+++ src/main/java/uta/cse3310/Wheel.java
@@ -1,9 +1,13 @@
 package uta.cse3310;
 
 public class Wheel {
-    private String[] item;
+    private String[] items = {"Nothing", "Bankrupt", "Free Spin", "Consonant Hint", "Vowel Hint",
+                              "100", "150", "200", "250", "300",
+                              "350", "400", "450", "500", "550",
+                              "600", "700", "800", "900", "1000"};
 
     public String getRandomItem() {
-        return null;
+        int randIndex = (int)(Math.random() * 20);
+        return items[randIndex];
     }
 }
